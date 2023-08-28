572d4604c0cba46836d6b332e7c79bcd930ecf26
iamsyahirah <syahirahmohammad89@gmail.com>
2023-08-28 16:57:25 +0800

    first time upload

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateEventRequest;
+use App\Models\Country;
+use App\Models\Event;
+use App\Models\Tag;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\View\View;
+use Illuminate\Support\Str;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+       
+        return view('events.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
+    {
+        $countries = Country::all();
+        $tags = Tag::all();
+        return view('events.create', compact('countries','tags'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CreateEventRequest $request)
+    {
+        if ($request->hasFile('image')) {
+            $data = $request->validated();
+            $data['image'] = Storage::putFile('events',$request->file('image'));
+            $data['user_id'] = auth()->id();
+            $data['slug'] = Str::slug($request->title);
+
+            $event = Event::create($data);
+            $event->tags()->attach($request->tags);
+            return to_route('events.index');
+        } else {
+            return back();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
